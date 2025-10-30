da292ab40dac044a906b9dd2bb49eb266fbdba60
Wasi Karan <brainmaster816@gmail.com>
2025-10-29 23:15:10 -0700

    frontend ready 2

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -127,10 +127,11 @@ const contact = () => {
                                                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3024.123456789!2d-74.0059413!3d40.7127753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDDCsDQyJzQ2LjAiTiA3NMKwMDAnMjEuNCJX!5e0!3m2!1sen!2sus!4v1234567890123"
                                                     width="100%"
                                                     height="400"
-                                                    style="border:0;"
-                                                    allowfullscreen=""
+style={{ border: 0 }}                                                    allowfullscreen=""
                                                     loading="lazy"
-                                                    referrerpolicy="no-referrer-when-downgrade">
+                                                    referrerpolicy="no-referrer-when-downgrade"
+                                                    title="Google Maps Location"
+                                                    >
                                                 </iframe>
                                             </div>
                                         </section>
